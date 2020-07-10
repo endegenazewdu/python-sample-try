fe66bd970d2c49cf1f2415595b62569a271aa7e6
endegenazewdu <ende_ze@yahoo.com>
2020-07-10 10:18:53 +0300

    loop

---
+++ conditional elif.py
@@ -0,0 +1,26 @@
+x=45
+y=4
+if x<=y:
+ print ("A")
+elif x>=y:
+ print ("cool")
+elif x==y:
+ print("done")
+else:
+ print ("B")
+# please student put your mark 
+mark= int(input("please entre the mark "))
+if mark>=90:
+  grade=("A")
+elif mark>=80:
+  grade=("B") 
+elif mark>=70:
+  grade=("C")
+elif mark>=60:
+  grade=("D")
+else:
+  grade=("F")
+print(grade)  
+  
+
+  
---
+++ loop.py
@@ -0,0 +1,13 @@
+i=1
+while i<7:
+ print (i)
+ i+=1
+ if i==5:#if i is equal 5 make break or stop
+    break
+print ("while and else working together")
+g=1
+while g<7:
+ print (g)
+ g+=1
+else:
+    print ("this number is big")   
